ac2b6f85b701c89280550a4967d7688f77f0a1ac
James <jamesncox@yahoo.com>
2019-08-28 17:21:41 -0500

    Password validation user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,12 @@ class User < ApplicationRecord
                        :confirmation => true,
                        :length => { minimum: 6 },
                        :unless => :already_has_password?
-                       
+
     has_secure_password
+
+    private
+
+    def already_has_password?
+      !self.password_digest.blank?
+    end
 end
