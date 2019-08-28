229c79105ec02e310933de9738a24151fe7dea7c
James <jamesncox@yahoo.com>
2019-08-28 17:22:43 -0500

    Add validations to recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,7 @@ class Recipe < ApplicationRecord
     belongs_to :user
     has_many :recipe_ingredients
     has_many :ingredients, through: :recipe_ingredients
+
+    validates :name, uniqueness: true
+    validates :name, presence: true
 end
