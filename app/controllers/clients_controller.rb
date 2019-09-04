a8514c5e4d795c01b51978a7ea6d2b202d02948a
James <jamesncox@yahoo.com>
2019-09-04 08:50:26 -0500

    Stubbed Client controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,30 @@
 class ClientsController < ApplicationController
+    def show 
+        @client = Client.find(params[:id])
+    end 
+
+    def index
+        @clients = Client.all
+    end
+
+    def new
+        @client = Client.new
+    end
+
+    def create
+        @client = Client.create(client_params)
+        redirect_to client_path
+    end 
+
+    def edit
+    end
+
+    def delete
+    end 
+
+    private
+        def client_params
+            params.require[:client].permit[:name]
+        end
+
 end
