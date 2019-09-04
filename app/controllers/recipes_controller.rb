1a08f20b9281678030c5d4c6f176e812917965e3
James <jamesncox@yahoo.com>
2019-09-04 08:08:23 -0500

    Added Nested Params to Recipe Controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,6 +10,8 @@ class RecipesController < ApplicationController
 
     def new 
         @recipe = Recipe.new
+        @recipe.ingredients.build
+        #line 13 is not ideal from what it sounds like even though it appears to work, ask DJ about it.
     end
 
     def create
@@ -28,6 +30,21 @@ class RecipesController < ApplicationController
     private
         # nested form lab, adding ingredients attributes to recipe_params
         def recipe_params
-            params.require(:recipe).permit(:name, :serves, :sauce, :garnish, :instructions)
+            params.require(:recipe).permit(:name, :serves, :sauce, :garnish, :instructions,
+                ingredients_attributes: [
+                    :name, 
+                    :quantity,
+                    :size,
+                    :unit
+                ]
+            )
         end
 end
+# create_table "ingredients", force: :cascade do |t|
+#     t.string "name"
+#     t.string "quantity"
+#     t.datetime "created_at", null: false
+#     t.datetime "updated_at", null: false
+#     t.integer "size"
+#     t.string "unit"
+#   end
