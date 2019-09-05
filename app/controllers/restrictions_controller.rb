651d4ad4beaaf58927ad114709d619344e9e4d15
James <jamesncox@yahoo.com>
2019-09-05 10:47:05 -0500

    Add restrictions controller

--- app/controllers/restrictions_controller.rb
+++ app/controllers/restrictions_controller.rb
@@ -1,2 +1,49 @@
 class RestrictionsController < ApplicationController
+    def show 
+        @restriction = Restriction.find(params[:id])
+    end 
+
+    def index
+        @restrictions = Restriction.all
+    end
+
+    def new
+        @restriction = Restriction.new
+    end
+
+    def create
+        @restriction = Restriction.new(restriction_params)
+
+        if @restriction.save
+            redirect_to @restriction
+        else 
+            render :new 
+        end
+    end 
+
+    def edit
+        @restriction = Restriction.find(params[:id])
+    end
+
+    def update
+        @restriction = Restriction.find(params[:id])
+        @restriction.update(restriction_params)
+        redirect_to restriction_path(@restriction)
+    end 
+
+    def destroy
+        @restriction = Restriction.find(params[:id])
+        @restriction.destroy
+        #binding.pry
+        redirect_to restrictions_path
+      end
+      
+    private
+        def restriction_params
+            params.require(:restriction).permit(:name,
+                restrictions_attributes: [
+                    :name
+                ]
+            )
+        end
 end
