53332aeea2c6cae7e1c010932f38ca7ff10220fc
github username <github email address>
2019-09-09 10:59:35 -0600

    debug sesh

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,8 @@ class SessionsController < ApplicationController
         session[:user_id] = @user.id
         redirect_to root_path
       else
-        redirect_to new_user_path
+        flash[:message] = "Login failed."
+        redirect_to login_path
       end
     end
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
       else 
         # flash[:notice] = "Form is invalid"
         # flash[:color]= "invalid"
-        redirect_to new_user_path
+        render :new
       end
     end
   
