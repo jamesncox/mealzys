e8fb12b18369139dab12243844eff32c8aafe774
jamesncox <jamesncox@yahoo.com>
2019-09-09 14:04:41 -0500

    .

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,24 @@
 class UsersController < ApplicationController
-    def new
+  def new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      # flash[:notice] = "You signed up successfully"
+      # flash[:color]= "valid"
+      redirect_to root_path
+    else 
+      # flash[:notice] = "Form is invalid"
+      # flash[:color]= "invalid"
+      render :new
     end
-  
-    def create
-      #my sessions or user controller is broken and won't send my user to new signup page...forms look fine.
-      @user = User.new(user_params)
-      if @user.save
-        session[:user_id] = @user.id
-        # flash[:notice] = "You signed up successfully"
-        # flash[:color]= "valid"
-        redirect_to root_path
-      else 
-        # flash[:notice] = "Form is invalid"
-        # flash[:color]= "invalid"
-        redirect_to login_path
-      end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :password, :password_confirmation)
     end
-  
-    private
-  
-      def user_params
-        params.require(:user).permit(:name, :password, :password_confirmation)
-      end
 end
