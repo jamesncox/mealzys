da59fe4eec6d10fbf65d14673efc6d2b55da3b62
jamesncox <jamesncox@yahoo.com>
2019-09-12 17:22:17 -0500

    Clean up routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,22 +4,9 @@ Rails.application.routes.draw do
   resources :recipes
   resources :clients
 
-  # get '/login' => 'sessions#new'
-  # post '/login' => 'sessions#create'
-  # delete '/logout' => 'sessions#destroy'
-
-  # the following is for omniauth-google routes
-  get 'login', to: redirect('/auth/google_oauth2'), as: 'login'
-  get 'logout', to: 'sessions#destroy', as: 'logout'
-  get 'auth/:provider/callback', to: 'sessions#create'
-  get 'auth/failure', to: redirect('/')
-  # get 'home', to: 'home#show'
-  # get 'me', to: 'me#show', as: 'me'
-
-  # get '/auth/:provider/callback' => 'sessions#create'
-  # get '/signin' => 'sessions#new', :as => :signin
-  # get '/signout' => 'sessions#destroy', :as => :signout
-  # get '/auth/failure' => 'sessions#failure'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
 
   root 'welcome#home'
 end
