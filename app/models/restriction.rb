58459893c6478f6de17163e96312b3149c29dad4
jamesncox <jamesncox@yahoo.com>
2019-09-21 09:26:38 -0500

    Debug Restriction custom validation

--- app/models/restriction.rb
+++ app/models/restriction.rb
@@ -1,10 +1,10 @@
 class Restriction < ApplicationRecord
     has_many :client_restrictions
     has_many :clients, through: :client_restrictions
-
-    # validates :name, uniqueness: {message: "Please select an existing restriction from the drop down menu."}
-
-    #create custom method to find_or_create_by etc...
+    
+    validate :unique_restriction
+    
+    private 
 
     def unique_restriction
         @restriction = Restriction.find_or_create_by(name: name)
